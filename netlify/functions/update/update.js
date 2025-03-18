c66031e6b6b7bccd0d0d83bc111b5a47d634b3b2
tripv-hash <tripvis12@gmail.com>
2025-03-18 17:10:24 +0100

    Fix 1 versione

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -3,8 +3,22 @@ const axios = require("axios").default;
 exports.handler = async (event) => {
   console.log("Received an update from Telegram:", event.body);
 
-  const body = JSON.parse(event.body);
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (error) {
+    console.error("Errore nel parsing del corpo dell'evento:", error);
+    return { statusCode: 400, body: JSON.stringify({ message: "Errore nel parsing del corpo dell'evento" }) };
+  }
+
   const message = body.message;
+
+  // Verifica che il messaggio e il campo chat esistano
+  if (!message || !message.chat) {
+    console.error("Messaggio o campo 'chat' mancante.");
+    return { statusCode: 400, body: JSON.stringify({ message: "Messaggio o campo 'chat' mancante." }) };
+  }
+
   const chatId = message.chat.id;
 
   // Comando /start: Risposta di benvenuto
